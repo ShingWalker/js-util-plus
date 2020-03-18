c68ea8e3c0c1124c99d2cec04a988e7405e9696c
beast <939212596@qq.com>
2020-03-18 18:02:51 +0800

    update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+
+/**
+ * 获取url上的值 
+ * @param name 
+ */
+export const getQueryString = (name: string): string => {
+  let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+  let r = window.location.search.substr(1).match(reg);
+  if (r != null) {
+      return unescape(r[2]);
+  };
+  return '';
+}
+
+/**
+ * 获取token
+ * @param cname
+ */
+export const getCookie = (cname: string) :string => {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i].trim();
+    if (c.indexOf(name) === 0) return c.substring(name.length, c.length);
+  }
+  return '';
+};
