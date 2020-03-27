78d41346b355f53de03b252eb7ace689efb3f0c6
beast <939212596@qq.com>
2020-03-27 14:54:55 +0800

    update

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,11 +1,5 @@
 import * as util from '../index';
 
-document.cookie = 'a = 1'
-
 test('strCheck', () => {
   expect(util.strCheck('18268100546', 'phone')).toBe(true);
-});
-
-test('cookieGet', () => {
-  expect(util.cookieGet("a")).toBe(1);
 });
