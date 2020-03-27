011cf53be3f477fded8a87b488fc39f98aa90a43
beast <939212596@qq.com>
2020-03-27 16:28:24 +0800

    update

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -3,3 +3,11 @@ import * as util from '../index';
 test('strCheck', () => {
   expect(util.strCheck('18268100546', 'phone')).toBe(true);
 });
+
+test('arrIsNull', () => {
+  expect(util.arrIsNull([])).toBe(false);
+});
+
+test('objIsNull', () => {
+  expect(util.objIsNull({})).toBe(false);
+})
