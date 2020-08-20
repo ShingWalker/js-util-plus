b05747a1f337181235839caacaac6462d3cd679a
beast <939212596@qq.com>
2020-08-20 20:56:58 +0800

    feature-新增树结构相关方法

--- src/tree/index.ts
+++ src/tree/index.ts
@@ -3,7 +3,9 @@
  * @param tree 树结构
  * @param func 函数
  */
-export const treeForeach = (tree: any[], func: Function) => {
+import any = jasmine.any;
+
+export const treeForeach = (tree: any[], func: any): void => {
   let node, list = [...tree]
   while (node = list.shift()) {
     func(node)
@@ -16,7 +18,7 @@ export const treeForeach = (tree: any[], func: Function) => {
  * @param tree 树结构
  * @param func 函数
  */
-export const treeForeachDepthDesc = (tree: any[], func: Function) => {
+export const treeForeachDepthDesc = (tree: any[], func: any):void => {
   tree.forEach(data => {
     func(data)
     data.children && treeForeachDepthDesc(data.children, func) // 遍历子树
@@ -28,7 +30,7 @@ export const treeForeachDepthDesc = (tree: any[], func: Function) => {
  * @param tree 树结构
  * @param func 函数
  */
-export const treeForeachDepthAsc = (tree: any[], func: Function) => {
+export const treeForeachDepthAsc = (tree: any[], func: any):void => {
   tree.forEach(data => {
     data.children && treeForeachDepthAsc(data.children, func) // 遍历子树
     func(data)
@@ -41,7 +43,7 @@ export const treeForeachDepthAsc = (tree: any[], func: Function) => {
  * @param id   id名
  * @param pid  pic名
  */
-export const listToTree = (list: any[], id: string = "id" , pid: string = "pid") => {
+export const listToTree = (list: any[], id: string = "id" , pid: string = "pid"): any[] => {
   let info = list.reduce((map, node) => (map[node[id]] = node, node.children = [], map), {})
   return list.filter(node => {
     info[node[pid]] && info[node[pid]].children.push(node)
@@ -54,7 +56,7 @@ export const listToTree = (list: any[], id: string = "id" , pid: string = "pid")
  * @param tree
  * @param func
  */
-export const treeFilter = (tree: any[] , func: Function ) => {
+export const treeFilter = (tree: any[] , func: any): any[] => {
   // 使用map复制一下节点，避免修改到原树
   return tree.map(node => ({ ...node })).filter(node => {
     node.children = node.children && treeFilter(node.children, func)
@@ -68,7 +70,7 @@ export const treeFilter = (tree: any[] , func: Function ) => {
  * @param func
  * @param path 路径有默认值
  */
-export function treeFindPath (tree: any[], func: Function, path = []) {
+export function treeFindPath (tree: any[], func: any, path = []): any {
   if (!tree) return []
   for (const data of tree) {
     path.push(data.id)
