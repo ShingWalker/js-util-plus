ebe344bec45b3f3fdc1b551b795f850a559ac1d5
beast <939212596@qq.com>
2020-08-20 20:33:35 +0800

    feature-新增树结构相关方法

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
 import { arrIsNull } from './array/index';
 import { objIsNull } from './object/index';
 import { strCheck, strTransformName } from './string/index';
-import { storeCookieSet, storeCookieGet, storeCookieDelete, storeCookieRemove, storeLocalStorageSet, storeLocalStorageGet } from './store/index';
+import {
+  storeCookieSet,
+  storeCookieGet,
+  storeCookieDelete,
+  storeCookieRemove,
+  storeLocalStorageSet,
+  storeLocalStorageGet,
+} from './store/index';
 import { uriGetParam } from './uri/index';
 import { browserGetType, browserIsNew } from './browser/index';
 import { ulg } from './utils/index';
+import {
+  treeFilter,
+  listToTree,
+  treeFindPath,
+  treeForeach,
+  treeForeachDepthAsc,
+  treeForeachDepthDesc,
+} from './tree/index';
 
 export {
   arrIsNull,
@@ -21,4 +36,10 @@ export {
   ulg,
   browserGetType,
   browserIsNew,
-}
+  treeFilter,
+  listToTree,
+  treeFindPath,
+  treeForeach,
+  treeForeachDepthAsc,
+  treeForeachDepthDesc,
+};
---
+++ src/tree/index.ts
@@ -0,0 +1,84 @@
+/**
+ * 广度优先遍历
+ * @param tree 树结构
+ * @param func 函数
+ */
+export const treeForeach = (tree: any[], func: Function) => {
+  let node, list = [...tree]
+  while (node = list.shift()) {
+    func(node)
+    node.children && list.push(...node.children)
+  }
+}
+
+/**
+ * 深度优先遍历(先序遍历)
+ * @param tree 树结构
+ * @param func 函数
+ */
+export const treeForeachDepthDesc = (tree: any[], func: Function) => {
+  tree.forEach(data => {
+    func(data)
+    data.children && treeForeachDepthDesc(data.children, func) // 遍历子树
+  })
+}
+
+/**
+ * 深度优先遍历(后序遍历)
+ * @param tree 树结构
+ * @param func 函数
+ */
+export const treeForeachDepthAsc = (tree: any[], func: Function) => {
+  tree.forEach(data => {
+    data.children && treeForeachDepthAsc(data.children, func) // 遍历子树
+    func(data)
+  })
+}
+
+/**
+ * 列表结构转树结构
+ * @param list 数组对象
+ * @param id   id名
+ * @param pid  pic名
+ */
+export const listToTree = (list: any[], id: string = "id" , pid: string = "pid") => {
+  let info = list.reduce((map, node) => (map[node[id]] = node, node.children = [], map), {})
+  return list.filter(node => {
+    info[node[pid]] && info[node[pid]].children.push(node)
+    return !node[pid]
+  })
+}
+
+/**
+ * 向上筛选树结构, 返回包含的树
+ * @param tree
+ * @param func
+ */
+export const treeFilter = (tree: any[] , func: Function ) => {
+  // 使用map复制一下节点，避免修改到原树
+  return tree.map(node => ({ ...node })).filter(node => {
+    node.children = node.children && treeFilter(node.children, func)
+    return func(node) || (node.children && node.children.length)
+  })
+}
+
+/**
+ * 向上查找树节路径, 返回节点id的数组
+ * @param tree
+ * @param func
+ * @param path 路径有默认值
+ */
+export function treeFindPath (tree: any[], func: Function, path = []) {
+  if (!tree) return []
+  for (const data of tree) {
+    path.push(data.id)
+    if (func(data)) return path
+    if (data.children) {
+      const findChildren = treeFindPath(data.children, func, path)
+      if (findChildren.length) return findChildren
+    }
+    path.pop()
+  }
+  return []
+}
+
