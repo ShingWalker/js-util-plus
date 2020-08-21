8f936420c1400d486ce0d931325945dee9dffc3a
beast <939212596@qq.com>
2020-08-21 09:30:44 +0800

    feature-tree相关代码

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,5 +1,43 @@
 import * as util from '../index';
 
+let tree = [
+  {
+    id: '1',
+    title: '节点1',
+    children: [
+      {
+        id: '1-1',
+        title: '节点1-1',
+        children: [
+          {
+            id: '1-1-1',
+            title: '节点1-1-1'
+          },
+          {
+            id: '1-1-2',
+            title: '节点1-1-2'
+          }
+        ]
+      },
+      {
+        id: '1-2',
+        title: '节点1-2'
+      }
+    ]
+  },
+  {
+    id: '2',
+    title: '节点2',
+    children: [
+      {
+        id: '2-1',
+        title: '节点2-1'
+      }
+    ]
+  }
+]
+
+
 test('strCheck', () => {
   expect(util.strCheck('18268100546', 'phone')).toBe(true);
 });
@@ -11,3 +49,26 @@ test('arrIsNull', () => {
 test('objIsNull', () => {
   expect(util.objIsNull({})).toBe(false);
 });
+
+
+test('treeForeach', ()=> {
+  expect(util.treeForeach(tree, node=>console.log(node)));
+})
+
+test('treeForeachDepthAsc', ()=> {
+  expect(util.treeForeachDepthAsc(tree, node=>console.log(node)));
+})
+
+test('treeForeachDepthDesc', ()=> {
+  expect(util.treeForeachDepthDesc(tree, node=>console.log(node)));
+})
+
+test('treeFilter', ()=> {
+  expect(util.treeFilter(tree, node=>node==='1-1-1'));
+})
+
+test('treeFindPath', ()=> {
+  expect(util.treeFindPath(tree, node=>node==='1-1-1'));
+})
+
+
